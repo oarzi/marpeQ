8c424b6ecf7d763523f7097d1671a3a2d72efb24
Daniel Lomazov <lomazov@campus.technion.ac.il>
2022-02-14 23:14:11 +0200

    pennylane101_100_OrderMatters_template - DONE.

--- QHack-master/Coding_Challenges/pennylane101_100_OrderMatters_template/order_matters_template.py
+++ QHack-master/Coding_Challenges/pennylane101_100_OrderMatters_template/order_matters_template.py
@@ -5,6 +5,18 @@ import pennylane as qml
 from pennylane import numpy as np
 
 
+def xy_qfunc(params):
+    qml.RX(params[0], wires=0)
+    qml.RY(params[1], wires=0)
+    return qml.expval(qml.PauliX(0))
+
+
+def yx_qfunc(params):
+    qml.RY(params[1], wires=0)
+    qml.RX(params[0], wires=0)
+    return qml.expval(qml.PauliX(0))
+
+
 def compare_circuits(angles):
     """Given two angles, compare two circuit outputs that have their order of operations flipped: RX then RY VERSUS RY then RX.
 
@@ -18,7 +30,14 @@ def compare_circuits(angles):
     # QHACK #
 
     # define a device and quantum functions/circuits here
-    ofir was here
+    dev = qml.device("default.qubit", wires=1)
+
+    # define an executable quantum node as a circuit applying RX followed by RY and vice versa
+    xy_qnode = qml.QNode(func=xy_qfunc, device=dev)
+    yx_qnode = qml.QNode(func=yx_qfunc, device=dev)
+
+    res = xy_qnode(angles) - yx_qnode(angles)
+    return res.abs()
 
     # QHACK #
 
