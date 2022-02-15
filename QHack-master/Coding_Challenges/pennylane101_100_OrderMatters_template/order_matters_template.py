a8604f56e116a85b35343d75873b4606c30f719e
oarzi <ofir.arzi@gmail.com>
2022-02-15 12:22:18 +0200

    pnln101_100_ofir_sol

--- QHack-master/Coding_Challenges/pennylane101_100_OrderMatters_template/order_matters_template.py
+++ QHack-master/Coding_Challenges/pennylane101_100_OrderMatters_template/order_matters_template.py
@@ -4,6 +4,38 @@ import sys
 import pennylane as qml
 from pennylane import numpy as np
 
+def circuit(gates, gatewires, mes, meswires):
+    def ret_circ(gateparams):
+        for gate, params, wire in zip(gates, gateparams, gatewires):
+            gate(params, wires=wire)
+        return qml.expval(mes(meswires))
+
+    return qml.expval(qml.PauliX(0))
+
+
+    return ret_circ
+
+def compare_circuits_ofir(angles):
+    """Given two angles, compare two circuit outputs that have their order of operations flipped: RX then RY VERSUS RY then RX.
+
+    Args:
+        - angles (np.ndarray): Two angles
+
+    Returns:
+        - (float): | < \sigma^x >_1 - < \sigma^x >_2 |
+    """
+
+    # QHACK #
+    # define a device and quantum functions/circuits here
+    dev = qml.device("default.qubit", wires=1)
+
+    # define an executable quantum node as a circuit applying RX followed by RY and vice versa
+    xy_qnode = qml.QNode(func=xy_qfunc, device=dev)
+    yx_qnode = qml.QNode(func=yx_qfunc, device=dev)
+
+    res = xy_qnode(angles) - yx_qnode(angles)
+    return res.abs()
+
 
 def xy_qfunc(params):
     qml.RX(params[0], wires=0)
@@ -18,27 +50,13 @@ def yx_qfunc(params):
 
 
 def compare_circuits(angles):
-    """Given two angles, compare two circuit outputs that have their order of operations flipped: RX then RY VERSUS RY then RX.
+    c1 = circuit([qml.RX, qml.RY], [0, 0], qml.PauliX, 0)
+    c2 = circuit([qml.RY, qml.RX], [0, 0], qml.PauliX, 0)
 
-    Args:
-        - angles (np.ndarray): Two angles
-
-    Returns:
-        - (float): | < \sigma^x >_1 - < \sigma^x >_2 |
-    """
-
-    # QHACK #
-
-    # define a device and quantum functions/circuits here
-    dev = qml.device("default.qubit", wires=1)
-
-    # define an executable quantum node as a circuit applying RX followed by RY and vice versa
-    xy_qnode = qml.QNode(func=xy_qfunc, device=dev)
-    yx_qnode = qml.QNode(func=yx_qfunc, device=dev)
-
-    res = xy_qnode(angles) - yx_qnode(angles)
-    return res.abs()
-
+    qnode1 = qml.QNode(c1, dev)(angles)
+    qnode2 = qml.QNode(c2, dev)(list(reversed(angles)))
+    
+    return np.absolute(qnode1 - qnode2)
     # QHACK #
 
 
